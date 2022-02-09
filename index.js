71b966c7e9867016b7edb763b861139c46b00916
unknown <arifp57895698@gmail.com>
2022-02-10 03:07:16 +0600

    more api added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ffgwy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.ffgwy.mongodb.net:27017,cluster0-shard-00-01.ffgwy.mongodb.net:27017,cluster0-shard-00-02.ffgwy.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-skeod6-shard-0&authSource=admin&retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -22,6 +22,8 @@ async function run() {
       await client.connect();
       const database = client.db("Emarket");
       const productsCollection = database.collection("products");
+      const ordersCollection=database.collection("orders")
+      const usersCollection=database.collection("users")
       // query for movies that have a runtime less than 15 minutes
       
       app.get('/products', async(req,res)=>{
@@ -29,6 +31,12 @@ async function run() {
         const result= await cursor.toArray()
         res.send(result)
     })
+
+    app.post('/products', async(req,res)=>{
+      const product = req.body;
+      const result = await productsCollection.insertOne(product)
+      res.json(result)
+  })
 
     app.get('/products/:id',async(req,res)=>{
       const id=req.params.id
@@ -38,7 +46,7 @@ async function run() {
       res.send(result)
     })
 
-
+    //  getting product by category api
     app.get('/productbycategory',async(req,res)=>{
       const category=req.query.category
       const query={category:category}
@@ -46,6 +54,98 @@ async function run() {
       const result=await cursor.toArray()
       res.send(result)
     })
+
+
+    // post order api
+
+    app.post('/orders', async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order)
+
+      res.send(result)
+      
+   
+    })
+
+    app.get('/orders', async (req, res) => {
+      const email = req.query.email;
+      console.log(email)
+      const query={email:email}
+      const cursor =  ordersCollection.find(query)
+      const result= await cursor.toArray()
+      console.log(result)
+
+      res.send(result)
+      
+   
+    })
+
+    app.get('/totalorder',async(req,res)=>{
+      const cursor=ordersCollection.find({})
+      const result= await cursor.toArray()
+      res.send(result)
+    })
+
+
+
+    app.put('/orders/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: ObjectId(id) }
+      const doc = req.body
+      const updateDoc = { $set: doc }
+      const result = await ordersCollection.updateOne(filter, updateDoc)
+      
+    })
+
+
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: ObjectId(id) }
+
+      const result = await ordersCollection.deleteOne(filter)
+      console.log(result)
+    })
+
+
+    app.put('/saveuser',async(req,res)=>{
+      const user = req.body;
+        console.log(user)
+        const filter = { email: user.email }
+        const options = { upsert: true }
+        const updateDoc = { $set: user }
+        const result = await usersCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+    })
+
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const filter = { email }
+      const result = await usersCollection.findOne(filter)
+      let isAdmin = false;
+      if (result?.role == 'admin') {
+        isAdmin = true
+      }
+      res.json({ admin: isAdmin })
+    })
+
+    app.put('/users/admin',  async (req, res) => {
+      const email = req.body
+      const filter = { email: email.email };
+     
+     
+       
+          const updateDoc={
+            $set:{
+              role:'admin'
+            }
+          }
+
+          const result= await usersCollection.updateOne(filter,updateDoc)
+          res.json(result)
+        
+        })
+
+
     } finally {
     //   await client.close();
     }
